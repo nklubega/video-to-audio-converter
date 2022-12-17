2659a4cf3e787dd8df22765b8a61a319fea03ed5
Nabil Lubega <nklubega@gmail.com>
2022-12-17 08:21:04 +0300

    added init, jsontoken, requirements and server

---
+++ src/auth/jsontoken.py
@@ -0,0 +1,13 @@
+import jwt, datetime
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+
+        },
+        secret,
+        algorithm="HS256",
+    )
