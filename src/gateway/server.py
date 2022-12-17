c87aa7c0dbabb3ed8fe7c9cbc62a9ae040a48848
Nabil Lubega <nklubega@gmail.com>
2022-12-17 08:25:49 +0300

    Add files via upload

---
+++ src/gateway/server.py
@@ -0,0 +1,32 @@
+import os, gridfs, pika, json
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+
+server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
+
+mongo = PyMongo(server)
+
+fs = gridfs.GridFS(mongo.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+@server.route('/login', methods=["POST"])
+def login():
+    token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
+
+#uploading the video
+@server.route('/upload', methods=['POST'])
+def upload():
+    access, err = validate.token(request)
+
+    acc
