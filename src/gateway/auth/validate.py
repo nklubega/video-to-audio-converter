bcaf6692f827a94fa3ca4b4b4ebbcb9e0951b3d8
Nabil Lubega <nklubega@gmail.com>
2022-12-22 07:18:35 +0300

    Updated validate.py

--- src/gateway/auth/validate.py
+++ src/gateway/auth/validate.py
@@ -10,5 +10,12 @@ def token(request):
         return None, ("Missing credentials", 401)
 
     response = requests.post(
+        f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/validate",
+        headers= {"Authorization": token},
         
     )
+
+    if response.status_code == 200:
+        return response.txt, None
+    else:
+        return None, (response.txt, response.status_code)
